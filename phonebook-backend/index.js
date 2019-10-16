67dd5e76a091fad6cad47dfffa401a7315b4c5e5
divpusher <hello@divpusher.com>
2019-10-16 22:40:48 +0200

    exercise 3.4

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -44,6 +44,14 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+
 app.get('/info', (req, res) => {
   res.end(
     `Phonebook has info for ${persons.length} people\n` + 
