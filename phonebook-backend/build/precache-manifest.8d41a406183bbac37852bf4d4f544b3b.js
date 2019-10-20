32fba145d668392dc883734fe2f1b1dd1a35e94d
divpusher <hello@divpusher.com>
2019-10-20 15:26:33 +0200

    exercise 3.11

---
+++ phonebook-backend/build/precache-manifest.8d41a406183bbac37852bf4d4f544b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514297a0f9e9e6b1224101ddbfd89c43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ae9f4584a4f0923fcc0",
+    "url": "/static/css/main.868e051f.chunk.css"
+  },
+  {
+    "revision": "bc91d37d1b4e857e9ea4",
+    "url": "/static/js/2.baafe9e2.chunk.js"
+  },
+  {
+    "revision": "5ae9f4584a4f0923fcc0",
+    "url": "/static/js/main.4a361a47.chunk.js"
+  },
+  {
+    "revision": "d92e7acfcbfa197868a5",
+    "url": "/static/js/runtime-main.d36c9b34.js"
+  }
+]);
--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 
+app.use(express.static('build'))
 
 app.use(cors())
-
 
 app.use(bodyParser.json())
 
