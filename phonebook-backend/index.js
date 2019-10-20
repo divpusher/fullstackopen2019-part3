4c6358893019d892dce60dff88473f6419f2ccb8
divpusher <hello@divpusher.com>
2019-10-20 23:39:31 +0200

    exercise 3.17

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -77,6 +77,23 @@ app.post('/api/persons', (request, response) => {
 
 
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+
+
 app.get('/info', (req, res) => {
   res.end(
     `Phonebook has info for ${persons.length} people\n` + 
