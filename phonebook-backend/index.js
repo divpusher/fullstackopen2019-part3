29bdd9a3a4242f58a02669cac54e5082a311aa29
divpusher <hello@divpusher.com>
2019-10-20 19:31:50 +0200

    exercise 3.13

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 
@@ -24,34 +26,11 @@ app.use(
   )
 
 
-let persons = [  
-  {    
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1 
-  },  
-  {    
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2 
-  },  
-  {    
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3 
-  },
-  {    
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4 
-  }
-]
 
-
-
-app.get('/api/persons', (req, res) => {
-  console.log()
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(people => {
+    response.json(people.map(person => person.toJSON()))
+  })
 })
 
 
